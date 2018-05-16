07ec1a347a2b1722a469e570e5a2329bd20f1187
Jiggly Jello <31894542+BombasticJam@users.noreply.github.com>
2018-05-16 14:16:32 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import secondtext as st
 
 class Game:
 
-  def pause():
-    input()
-
   bedroom = "bedroom"
   kitchen = "kitchen"
   party_room = "partyroom"
@@ -53,18 +50,23 @@ class Game:
   def __str__(self):
     return self.state
 
+#    person = input('Enter your name: ')
+#    print('Hello {}!'.format(person))
+
   def start(self):
-    welcome_1 = input("Hello,\nWelcome to my Game!\nPlease Enter Your Name:  ").format[self.player]
-    print(welcome_1)
-    pause()
-    welcome_2 = ("So its {} huh,\nWhat a Great Name of yours").format[self.player]
-    print(welcome_2)
-    pause()
-    welcome_3 = ("You should really get going {},\nYou have a party and guests waiting to be attended").format[self.player]
-    print(welcome_3)
-    pause()
-    lots_of_spaces = ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-    print(lots_of_spaces)
+    try:
+        person = input("Hello,\nWelcome to my Game!\nPlease Enter Your Name:  ").format(self.player)
+        print(person, "...").join(format(person))
+    except AttributeError:
+        st.pause()
+        welcome_1 = ("So its {} huh,\nWhat a Great Name of yours").format(person)
+        print(welcome_1)
+        st.pause()
+        welcome_2 = ("You should really get going {},\nYou have a party and guests waiting to be attended").format(person)
+        print(welcome_2)
+        st.pause()
+        lots_of_spaces = ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print(lots_of_spaces)
 a = Game()
 a.start()
 print(st.mid())
