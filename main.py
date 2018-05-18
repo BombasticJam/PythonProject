c0768d4ee9643ca5f72c984f54a7cffa1179d101
Jiggly Jello <31894542+BombasticJam@users.noreply.github.com>
2018-05-18 16:15:11 -0400

    Updated main.py
    Cleaned up the code

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-import secondtext as st
+ import secondtext as st
 
 class Game:
-
-  bedroom = "bedroom"
-  kitchen = "kitchen"
-  party_room = "partyroom"
-  dining_room = "diningroom"
-  backyard = "backyard"
-  bathroom = "bathroom"
-
-  rooms = [bedroom, kitchen, party_room, dining_room, backyard, bathroom]
-
-  henry = "henry"
-  liliana = "liliana"
-  joshua = "joshua"
-  aaron = "aaron"
-  todd = "todd"
-  bandana = "bandana"
-  gumwrappers = "gumwrappers"
-
-  people_in_rooms = {
-    bedroom:henry, kitchen:liliana, party_room:joshua, backyard: aaron, bathroom:todd
-  }
-
-  wallet_h = "*You see a picture of Henry's dog, stacy*"
-  cigarettes_l = "*I never knew that Liliana smoked, nor did I know she had enough money to buy any due to the loans she has been stacking up for her so on 'College Fees'*"
-
-
-  people_items = {
-    henry:wallet_h, liliana:cigarettes_l, joshua:bandana, aaron:gumwrappers
-  }
-
-
-
-
-
-  wallet_h = "*As you open the leather wallet, you see Henry's dog, Stacy, who just passed away recently"
-
 
   player = []
 
