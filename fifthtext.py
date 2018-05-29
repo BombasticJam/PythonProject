9a0e560909b22454d1824e64e026ee56ac5d9245
Jiggly Jello <31894542+BombasticJam@users.noreply.github.com>
2018-05-29 11:23:43 -0400

    created fifthtext.py

---
+++ fifthtext.py
@@ -0,0 +1,15 @@
+def pick2():
+    room_choice = []
+    rooms = {
+            "bedroom" : "Liliana\nBriana\nKevin",
+            "kitchen" : "Nicole\nHenry\nArnold\nAdrian\nCrystal"
+            "partyroom" : "Pamela\nLaisha\nArleen\nAndrew\nMishael\nJohn\nDaniel\nLanaya\nNatalie\nJacob"
+            "diningroom" : "Michael\nEnrico\nEdu\nJohnathan\nMatthew\nAmanda"
+            "backyard" : "June\nJuly\nDawn\nJulissa"
+            "bathroom" : "Jade"
+    }
+
+    #  Test Code
+    print(rooms["bedroom"])
+
+pick2()
