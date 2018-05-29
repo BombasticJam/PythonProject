634e519d0b3cd5a35f7ac8dc40ba5e82429492f5
Jiggly Jello <31894542+BombasticJam@users.noreply.github.com>
2018-05-28 20:16:49 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import secondtext as st
 import thirdtext as tt
+import fourthtext as ft
 
 class Game:
   def pause():
@@ -36,41 +37,10 @@ class Game:
         lots_of_spaces = ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         print(lots_of_spaces)
 
-  def secondmid(self):
-      s1 = ("Crystal:'Please... help'")
-      print(s1)
-      input()
-      s2 = ("Crystal:'It hurts.. inside'")
-      print(s2)
-      input()
 
-            #This code below will break but I will soon fix it
-      s3 = ((self.player, " : "), "'What happened to you?'").join()
-      print(s3)
-      input()
-      s4 = ((self.player, ": "), "'Did you fall or...'").join()
-      print(s4)
-      s5 = ("Crystal: 'No, i think it something was in my drink'")
-      print(s5)
-      input()
-      s6 = ("Crystal: 'I just can't s.. -*Cough*-'")
-      print(s6)
-      input()
-      s7 = ((self.player, ": "), "Just hold on tight love, I'll get some help quick")
-      print (s7)
-      input()
-      s8 = ("Crystal: 'No, don't leave -*Cough*- my side please.. I'm scared")
-      print(s8)
-      input()
-      s9 = "You see Crystal move side to side as she struggles to breath"
-      print(s9)
-      input()
-      s10 = "You try to perform CPR on her with all your might"
-      print(s10)
-      s11 = ("You soon fail and try to call for help")
-      print(s11)
-      input()
+
 a = Game()
 a.start()
 print(st.mid())
-print(a.secondmid(player))
+tt.secondmid()
+ft.thirdmid()
