83d464402a903031187588ffc03462e79d400823
Jiggly Jello <31894542+BombasticJam@users.noreply.github.com>
2018-05-30 11:26:26 -0400

    updated fifthtext.py

--- fifthtext.py
+++ fifthtext.py
@@ -1,7 +1,7 @@
 def pick2():
     room_choice = []
     rooms = {
-            "bedroom" : "Liliana\nBriana\nKevin",
+            "bedroom" : "Liliana\nBriana\nKevin\nHoward",
             "kitchen" : "Nicole\nHenry\nArnold\nAdrian\nCrystal",
             "partyroom" : "Pamela\nLaisha\nArleen\nAndrew\nMishael\nJohn\nDaniel\nLanaya\nNatalie\nJacob",
             "diningroom" : "Michael\nEnrico\nEdu\nJohnathan\nMatthew\nAmanda",
@@ -14,17 +14,37 @@ def pick2():
         "briana" : "purse\nreceipt\nphone\nmonopoly_money",
         "kevin" : "watch\npencil\nnotebook\nheadphones\nmonopoly_money",
         "nicole" : "toothpick\nempty_water_bottle\npurse\nbracelet",
+        "henry" : "keychain\nredcup\nwallet\nmintgum\ncigarettes",
+        "arnold" : "bandana\nwallet\nwatch\nmatches\nphone",
+        "adrian" : "dogtag\npurse\nwallet\nnametag\npieceofpaper\nphone",
+        "crystal" : "bloodypieceofpaper\npurse\ncrystalnecklace\nbracelet\nbody",
+        "pamela" : "necklace\nbracelet\npen\nwatch\npurse\nphone",
+        "laisha" : "deckofcards\nlipstick\npurse\nlolipop\nheadphones\nphone",
+        "arleen" : "waterbottle\nsmallbackpack\ncosmetic\nbubblegum\nphone",
+        "andrew" : "headband\nbeer\nphone\nkeys\nglasses"
+        "mishael" : "",
 
-
-#crystal will have a button which will be the trophy of the murder (The murder will have to contain her button)
+#crystal will have a button which will be the trophy of the murder (The murder will have to contain her button)  Crystal's purse's goods have been stolen so her purse will contain nothing
+#Pamela will have similar items that crystal will have because they are best friends
+#maybe the winner will be calling crystal's phone
+#pamela will have stuff in her purse such as a lolipop and otherthings
+#arleen will carry a backpack that will contain food
+#laisha got a lolipop from pamela (also great friends with pamela)
     }
 
     liliana = {
-        "wallet" : "As you look into her wallet, something fell out of it which made you muffle 'Deja vu' as you picked it up and looked at it. It was a white piece of plastic which looked like a gum wrapper. Ignoring this, you put your attention to the clear notice that you were trying to find poison and not try to make the scene bigger than it already was",
+        "wallet" : "As you look into her wallet, something fell out of it which made you muffle 'Deja vu' as you picked it up and looked at it. It was a white piece of plastic which looked like a gum wrapper. You put the gum wrapper into your pocket and search the wallet thorough but found nothing but credit cards.",
         "dice" : "Just some plain old white dice, nothing to be interested in",
-        "reciept" : ""
+        "reciept" : "Bought 4 bags of fruit and a limited edition monopoly gameboard",
+        "nmonopoly_money" : "1000 monopoly credits"
+    }
 
+    briana = {
+    "purse" : ""
+    }
 
+    kevin = {
+    "watch" : "",
 
     }
 
@@ -32,5 +52,6 @@ def pick2():
     #  Test Code
     print(rooms["bedroom"])
     print(people_items["kevin"])
+    print(kevin["watch"])
 
 pick2()
