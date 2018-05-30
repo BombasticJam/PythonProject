1b84f7a10f5fbf9ca7138fb068b6294e06f4bb5e
Jiggly Jello <31894542+BombasticJam@users.noreply.github.com>
2018-05-29 20:47:26 -0400

    updated fifthtext.py

--- fifthtext.py
+++ fifthtext.py
@@ -2,14 +2,35 @@ def pick2():
     room_choice = []
     rooms = {
             "bedroom" : "Liliana\nBriana\nKevin",
-            "kitchen" : "Nicole\nHenry\nArnold\nAdrian\nCrystal"
-            "partyroom" : "Pamela\nLaisha\nArleen\nAndrew\nMishael\nJohn\nDaniel\nLanaya\nNatalie\nJacob"
-            "diningroom" : "Michael\nEnrico\nEdu\nJohnathan\nMatthew\nAmanda"
-            "backyard" : "June\nJuly\nDawn\nJulissa"
+            "kitchen" : "Nicole\nHenry\nArnold\nAdrian\nCrystal",
+            "partyroom" : "Pamela\nLaisha\nArleen\nAndrew\nMishael\nJohn\nDaniel\nLanaya\nNatalie\nJacob",
+            "diningroom" : "Michael\nEnrico\nEdu\nJohnathan\nMatthew\nAmanda",
+            "backyard" : "June\nJuly\nDawn\nJulissa",
             "bathroom" : "Jade"
     }
 
+    people_items = {
+        "liliana" : "wallet\ndice\nreciept\nmonopoly_money",
+        "briana" : "purse\nreceipt\nphone\nmonopoly_money",
+        "kevin" : "watch\npencil\nnotebook\nheadphones\nmonopoly_money",
+        "nicole" : "toothpick\nempty_water_bottle\npurse\nbracelet",
+
+
+#crystal will have a button which will be the trophy of the murder (The murder will have to contain her button)
+    }
+
+    liliana = {
+        "wallet" : "As you look into her wallet, something fell out of it which made you muffle 'Deja vu' as you picked it up and looked at it. It was a white piece of plastic which looked like a gum wrapper. Ignoring this, you put your attention to the clear notice that you were trying to find poison and not try to make the scene bigger than it already was",
+        "dice" : "Just some plain old white dice, nothing to be interested in",
+        "reciept" : ""
+
+
+
+    }
+
+
     #  Test Code
     print(rooms["bedroom"])
+    print(people_items["kevin"])
 
 pick2()
